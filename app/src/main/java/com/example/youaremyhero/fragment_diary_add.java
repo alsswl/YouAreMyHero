adcfbad322ff39e1728627c8815039d0b164ffeb
[alsswl] <[alswl4223@naver.com]>
2023-01-26 00:58:07 +0900

    feat : 날짜선택 datepicker로 하는 기능

--- app/src/main/java/com/example/youaremyhero/fragment_diary_add.java
+++ app/src/main/java/com/example/youaremyhero/fragment_diary_add.java
@@ -1,21 +1,77 @@
 package com.example.youaremyhero;
 
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
+import java.util.Calendar;
+import java.util.Date;
+
+
 public class fragment_diary_add extends Fragment {
+
+
+    TextView dateText;
+    DatePickerDialog datePickerDialog;
+
+    @SuppressLint("MissingInflatedId")
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_diary_add,container,false);
+        ViewGroup v = (ViewGroup) inflater.inflate(R.layout.fragment_diary_add, container, false);
+
+        dateText = v.findViewById(R.id.dateText);
+        ImageButton datepickBtn = (ImageButton)v.findViewById(R.id.calendarButton);
+
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                dateText.setText(year + "년 " + (month+1) + "월 " + dayOfMonth + "일");
+            }
+        };
+
+        datepickBtn.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onClick(View v) {
+
+                Date currentDate = new Date();
+
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTime(currentDate);
+
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(getContext(), dateSetListener, year, month, day);
+                datePickerDialog.show();
+            }
+        });
+
+        return v;
+
+
     }
+
+
+
 }
